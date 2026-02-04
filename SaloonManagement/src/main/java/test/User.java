63b62202eefac2fe3af005cc505454a918452865
Swayambhu7890 <bhapkarswayambhu@gmail.com>
2026-02-04 15:40:35 +0530

    profile page updated

--- SaloonManagement/src/main/java/test/User.java
+++ SaloonManagement/src/main/java/test/User.java
@@ -1,26 +1,48 @@
 package test;
 
-public class User 
-{
-	private String name;
+public class User {
+
+    private String name;
     private String email;
-    private String contactNo;
-    private String username;
-    private String password;
+    private String phone;
+    private String address;
 
-    // Getters & Setters
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public User() 
+    {
+    	
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    
+    public String getName()
+    {
+        return name;
+    }
 
-    public String getContactNo() { return contactNo; }
-    public void setContactNo(String contactNo) { this.contactNo = contactNo; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public String getEmail() {
+        return email;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
